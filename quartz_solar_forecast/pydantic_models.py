6ebd244da906bba0fe18184c72cc76587f4d69d0
EdFage <87755165+EdFage@users.noreply.github.com>
2023-12-01 17:33:38 +0000

    swap latitude and longitude in description so it is correct

--- quartz_solar_forecast/pydantic_models.py
+++ quartz_solar_forecast/pydantic_models.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel, Field
 
 class PVSite(BaseModel):
 
-    latitude: float = Field(..., description="the longitude of the site", ge=-90, le=90)
-    longitude: float = Field(..., description="the latitude of the site", ge=-180, le=180)
+    latitude: float = Field(..., description="the latitude of the site", ge=-90, le=90)
+    longitude: float = Field(..., description="the longitude of the site", ge=-180, le=180)
     capacity_kwp: float = Field(..., description="the capacity [kwp] of the site", ge=0)
