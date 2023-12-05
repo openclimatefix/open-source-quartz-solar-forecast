3b0ca67260d2ae3203ab2c23b01a166845770d6f
EdFage <87755165+EdFage@users.noreply.github.com>
2023-12-05 14:18:44 +0000

    add test for getting nwp data

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,7 +8,7 @@ def test_get_gfs_nwp():
     # make input data
     site = PVSite(latitude=51.75, longitude=-1.25, capacity_kwp=1.25)
     ts = datetime(2023, 10, 30, 0, 0)
-    source = "gfs"
+    source = "dwd-icon"
 
     # get data
     data = get_gfs_nwp(site=site, ts=ts, source=source)    
