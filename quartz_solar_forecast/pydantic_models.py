40d7809d5d5134f3dfd596bfa36baf47de1e938c
EdFage <87755165+EdFage@users.noreply.github.com>
2023-12-07 11:21:12 +0000

    add tilt and orientation to PVSite

--- quartz_solar_forecast/pydantic_models.py
+++ quartz_solar_forecast/pydantic_models.py
@@ -6,3 +6,7 @@ class PVSite(BaseModel):
     latitude: float = Field(..., description="the latitude of the site", ge=-90, le=90)
     longitude: float = Field(..., description="the longitude of the site", ge=-180, le=180)
     capacity_kwp: float = Field(..., description="the capacity [kwp] of the site", ge=0)
+    tilt: float = Field(..., description="the tilt of the site [degrees], the panels' angle relative to horizontal ground", ge=0, le=90)
+    orientation: float = Field(..., description="the orientation of the site [degrees], the angle between north and the direction the panels face, measured on the horizontal plane."
+ , ge=0, le=360)
+
