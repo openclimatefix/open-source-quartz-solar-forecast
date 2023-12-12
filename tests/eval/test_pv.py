3bdfb295a11fb087a45544d68cd8ff067c3acb16
peterdudfield <peter.dudfield@hotmail.com>
2023-12-12 19:40:07 +0000

    add test for getting pv metadata

--- tests/eval/test_pv.py
+++ tests/eval/test_pv.py
@@ -1,8 +1,22 @@
-from quartz_solar_forecast.eval.pv import get_pv_truth
+from quartz_solar_forecast.eval.pv import get_pv_truth, get_pv_metadata
 import pandas as pd
 
 
-# can take ~ 1 minute to run
+def test_get_pv_metadata():
+    test_set_df = pd.DataFrame(
+        [
+            {
+                "timestamp": pd.Timestamp("2021-01-26 01:15:00"),
+                "latitude": 51.5,
+                "longitude": 0.0,
+                "pv_id": 8215,
+            }
+        ]
+    )
+
+    metadata_df = get_pv_metadata(test_set_df)
+
+
 def test_get_pv():
     # make test dataset file
     test_set_df = pd.DataFrame(
