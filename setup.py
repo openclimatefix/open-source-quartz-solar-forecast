879fa657ae90a3034ebb2bd784d1821aa26895cb
BumpVersion Action <bumpversion@github-actions>
2023-12-19 15:12:34 +0000

    Bump version: 0.0.1 → 0.0.2 [skip ci]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="quartz_solar_forecast",
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     author="Peter Dudfield",
     author_email="info@openclimatefix.org",
