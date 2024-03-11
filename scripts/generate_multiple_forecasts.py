5bab8f25d06fa2270d083cf0a34a7f8aec23d99a
Liam <davisliam123@gmail.com>
2024-03-11 17:50:20 -0400

    finished script to generate multiple forecasts and write to forecasts.csv

--- scripts/generate_multiple_forecasts.py
+++ scripts/generate_multiple_forecasts.py
@@ -9,50 +9,49 @@ through the forecaster then each forecast will be saved to a csv.
 # imports
 from quartz_solar_forecast.forecast import run_forecast
 from quartz_solar_forecast.pydantic_models import PVSite
-from datasets import load_dataset
 import pandas as pd
 
-# huggingface url
-site_data = load_dataset("openclimatefix/uk_pv")
+# metadata link
+site_data = "quartz_solar_forecast/dataset/metadata.csv"
 
 # open forecasts.csv to output
-file_path = "quartz_solar_forecast/dataset/forecasts.csv"
+output_path = "quartz_solar_forecast/dataset/forecasts.csv"
 
 # initialize list of pv ids
 pv_ids = [
-    9531
+    
 ]
 
 # initialize list of PV Sites
-sites = []
+sites = {}
 
 # get specific information given pv id
 def get_info(pv_id):
-    # Filter the site data for the given pv id
-    filtered_data = site_data.filter(lambda example: example['ss_id'] == pv_id)
-
-    # If there is no matching pv id in site data
-    if len(filtered_data) == 0:
-        return "No data found for the given ss_id."
-
-    # Extract the latitude, longitude and capacity
-    latitude = filtered_data['latitude_rounded'][0]
-    longitude = filtered_data['longitude_rounded'][0]
-    capacity = filtered_data['kwp'][0]
-
-    return [latitude, longitude, capacity]
+    df = pd.read_csv(site_data)
+    
+    # extract row of pv id from metadata
+    row = df[df['ss_id'] == pv_id]
+    return PVSite(
+        latitude=row['latitude_rounded'].values[0],
+        longitude=row['longitude_rounded'].values[0],
+        capacity_kwp=row['kwp'].values[0],
+        # would add tilt and orientation here
+    )
 
 # get site data from metadata
 for pv_id in pv_ids:
-    row = get_info(pv_id)
-    sites.append(PVSite(row[0], row[1], row[2]))
+    pv_site = get_info(pv_id)
+    
+    # add to sites
+    sites[pv_id] = pv_site
 
 # list for csv data
 csv_data = []
 
 # run forecast on multiple sites
-for site in sites:
+for pv_id in pv_ids:
     # run forecast for site
+    site = sites[pv_id]
     predictions_df = run_forecast(site=site)
     
     # iterate through forecast rows
@@ -68,6 +67,6 @@ csv_df = pd.DataFrame(csv_data, columns=['latitude', 'longitude',
                                        'time', 'power_wh'])
 
 # write to csv
-csv_df.to_csv(file_path, index=False)
+csv_df.to_csv(output_path, index=False)
     
 print("complete")
