8045b316154f7dcc9035b383b7f2d335fddeb60f
Liam <davisliam123@gmail.com>
2024-03-11 20:08:31 -0400

    finished script to generate multiple forecasts and testing

--- scripts/generate_multiple_forecasts.py
+++ scripts/generate_multiple_forecasts.py
@@ -1,8 +1,9 @@
-""" Make a script to generate forecasts for multiple sites.
+"""Make a script to generate forecasts for multiple sites.
 
-The idea is to generate forecasts for multiple sites and to combine them into a single csv. 
-A list with several sites containing pv_id, latitude, longitude and capacity will be passed
-through the forecaster then each forecast will be saved to a csv.
+The idea is to generate forecasts for multiple sites and to combine them into a 
+single csv. A list with several sites containing pv_id, latitude, longitude 
+and capacity will be passed through the forecaster then each forecast will be saved 
+to a csv.
 
 """
 
@@ -15,59 +16,78 @@ from quartz_solar_forecast.pydantic_models import PVSite
 site_data_path = "quartz_solar_forecast/dataset/metadata.csv"
 output_path = "quartz_solar_forecast/dataset/forecasts.csv"
 
-# retrieve specific pv_site data from metadata
+
 def get_pv_site_data(site_data_path, pv_ids):
+    """Retrieve specific pv_site data from metadata."""
     sites = {}
     df = pd.read_csv(site_data_path)
-    
+
     # iterate through pv ids
     for pv_id in pv_ids:
-        row = df[df['ss_id'] == pv_id]
-        
+        row = df[df["ss_id"] == pv_id]
+
         if not row.empty:
             # make new site
             site = PVSite(
-                latitude=row['latitude_rounded'].values[0],
-                longitude=row['longitude_rounded'].values[0],
-                capacity_kwp=row['kwp'].values[0],
+                latitude=row["latitude_rounded"].values[0],
+                longitude=row["longitude_rounded"].values[0],
+                capacity_kwp=row["kwp"].values[0],
                 # Add tilt and orientation here
             )
-            
+
             # add site to sites dictionary
             sites[pv_id] = site
         else:
             # skip pv_id if it isn't in metadata
             print(f"Warning: PV ID {pv_id} not found in metadata. Skipping.")
-            
+
     return sites
 
-# generate forecasts for pv_sites
+
 def generate_forecasts(sites, output_path):
+    """Generate forecasts for pv_sites."""
     csv_data = []
-    
+
     # run forecast for each pv site
     for pv_id, site in sites.items():
         predictions_df = run_forecast(site=site)
-        
+
         # add to csv data
         for index, row in predictions_df.iterrows():
-            csv_row = [pv_id, site.latitude, site.longitude, site.capacity_kwp,
-                       index.date(), index.time(), row['power_wh']]
+            csv_row = [
+                pv_id,
+                site.latitude,
+                site.longitude,
+                site.capacity_kwp,
+                index.date(),
+                index.time(),
+                row["power_wh"],
+            ]
             csv_data.append(csv_row)
-            
-    csv_df = pd.DataFrame(csv_data, columns=['pv_id', 'latitude', 'longitude',
-                                             'capacity_kwp', 'date',
-                                             'time', 'power_wh'])
-    
+
+    csv_df = pd.DataFrame(
+        csv_data,
+        columns=[
+            "pv_id",
+            "latitude",
+            "longitude",
+            "capacity_kwp",
+            "date",
+            "time",
+            "power_wh",
+        ],
+    )
+
     # write to csv
     csv_df.to_csv(output_path, index=False)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     pv_ids = [int(arg) for arg in sys.argv[1:]]
-    
+
     if not pv_ids:
         print("Please provide PV IDs as command-line arguments.")
         sys.exit(1)
-    
+
     sites = get_pv_site_data(site_data_path, pv_ids)
     generate_forecasts(sites, output_path)
--- tests/test_generate_multiple_forecasts.py
+++ tests/test_generate_multiple_forecasts.py
@@ -3,25 +3,35 @@ import pandas as pd
 import subprocess
 import random
 
+
 def test_generate_forecasts():
     # Generate 100 random PV IDs between 1 and 50000
     pv_ids = [random.randint(1, 50000) for _ in range(100)]
-    
+
     # Define the command to run your script
-    command = "python scripts/generate_multiple_forecasts.py " + " ".join(map(str, pv_ids))
+    command = "python scripts/generate_multiple_forecasts.py " + " ".join(
+        map(str, pv_ids)
+    )
 
     # Run your script
     subprocess.run(command, shell=True)
 
     # Check if the output file is created
-    assert os.path.exists("quartz_solar_forecast/dataset/forecasts.csv"), "Forecast CSV file not created."
+    assert os.path.exists(
+        "quartz_solar_forecast/dataset/forecasts.csv"
+    ), "forecast.csv doesn't exist"
 
     # Load the output file
     df = pd.read_csv("quartz_solar_forecast/dataset/forecasts.csv")
 
     # Check if the output file has the correct columns
-    expected_columns = ['latitude', 'longitude', 'capacity_kwp', 'date', 'time', 'power_wh']
-    assert all([col in df.columns for col in expected_columns]), "Some expected columns are missing in the output CSV file."
+    expected_columns = ["latitude", "longitude", "capacity_kwp", "date", "time", "power_wh"]
+    assert all(
+        [col in df.columns for col in expected_columns]
+    ), "missing columns in forecast.csv"
+    
+    # Delete everything in forecasts.csv
+    open("quartz_solar_forecast/dataset/forecasts.csv", "w").close()
 
 if __name__ == "__main__":
     test_generate_forecasts()
