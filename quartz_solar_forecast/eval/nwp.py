7e7999e026a2720d39a6693df3c04971b502f1ee
Rosheen Naeem <rosheennaeem4@gmail.com>
2024-03-12 08:31:24 +0100

    Evaluate (#83)
    * fixed the path to testset in evaluation script
    
    * debugging the get_nwp function
    
    * Added comment
    
    * removed unnecessary

--- quartz_solar_forecast/eval/nwp.py
+++ quartz_solar_forecast/eval/nwp.py
@@ -80,6 +80,10 @@ def get_nwp_for_one_timestamp_one_location(
     # fs = HfFileSystem()
     # print(fs.ls("datasets/openclimatefix/dwd-icon-eu/data/2022/4/11/", detail=False))
 
+    # Ensure timestamp is a pd.Timestamp object
+    if not isinstance(timestamp, pd.Timestamp):
+        timestamp = pd.to_datetime(timestamp)
+
     # round timestamp to 6 hours floor
     timestamp_floor = timestamp.floor("6h")
     date_and_hour, huggingface_file = make_hf_filename(timestamp_floor)
@@ -170,5 +174,3 @@ def get_nwp_for_one_timestamp_one_location(
         print(f"Getting NWP for {timestamp} {pv_id}. Progress: {100*progress}%")
 
     return df
-
-
