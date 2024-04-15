d30e3dc56591b779496728caeb7a9d1f17f56861
Jakob Elias Gebler <jakob.gebler@rwth-aachen.de>
2024-04-15 10:45:30 +0200

    chnage datetime selection in testfile to a relative formate of -2 days

--- tests/test_forecast.py
+++ tests/test_forecast.py
@@ -1,14 +1,16 @@
 from quartz_solar_forecast.forecast import run_forecast
 from quartz_solar_forecast.pydantic_models import PVSite
-
+from datetime import datetime, timedelta
 
 def test_run_forecast():
     # make input data
     site = PVSite(latitude=51.75, longitude=-1.25, capacity_kwp=1.25)
 
+    ts = datetime.today() - timedelta(days=2)
+
     # run model with icon and gfs nwp
-    predications_df_gfs = run_forecast(site=site, ts='2023-12-30', nwp_source="gfs")
-    predications_df_icon = run_forecast(site=site, ts='2023-12-30', nwp_source="icon")
+    predications_df_gfs = run_forecast(site=site, ts=ts, nwp_source="gfs")
+    predications_df_icon = run_forecast(site=site, ts=ts, nwp_source="icon")
 
     print("\nPrediction based on GFS NWP\n")
     print(predications_df_gfs)
