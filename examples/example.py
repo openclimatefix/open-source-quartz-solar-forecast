f9922577b404832d83f188fa30e4ec6ea35e2160
Jakob Gebler <jakobgebler@mail.de>
2024-04-23 11:54:41 +0200

    Update example.py
    change the example to a dynamic timestamp

--- examples/example.py
+++ examples/example.py
@@ -1,17 +1,17 @@
 """ Example code to run the forecast"""
 from quartz_solar_forecast.forecast import run_forecast
 from quartz_solar_forecast.pydantic_models import PVSite
-
+from datetime import datetime, timedelta
 
 def main():
     # make input data
     site = PVSite(latitude=51.75, longitude=-1.25, capacity_kwp=1.25)
+    
+    ts = datetime.today() - timedelta(weeks=1)
+    predications_df = run_forecast(site=site, ts=ts, nwp_source="icon")
 
-    # run model
-    predictions_df = run_forecast(site=site, ts="2023-10-30")
-
-    print(predictions_df)
-    print(f"Max: {predictions_df['power_wh'].max()}")
+    print(predications_df)
+    print(f"Max: {predications_df['power_wh'].max()}")
 
 
 if __name__ == "__main__":
