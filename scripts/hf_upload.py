c213474eaa9653f5fbbd920d6d195b3fc5df4cba
Matthew Duffin <mduffin95@users.noreply.github.com>
2024-07-23 22:45:14 +0100

    use UTC

--- scripts/hf_upload.py
+++ scripts/hf_upload.py
@@ -1,7 +1,6 @@
-import datetime
 import os
-from huggingface_hub import login
-from huggingface_hub import HfFileSystem
+from datetime import datetime
+from huggingface_hub import login, HfFileSystem
 import forecast_csv
 
 if __name__ == "__main__":
@@ -9,7 +8,7 @@ if __name__ == "__main__":
     hf_token = os.getenv("HF_TOKEN")
     hf_repo = os.getenv("HF_REPO")
 
-    now = datetime.datetime.now()
+    now = datetime.utcnow()
     forecast = forecast_csv.forecast_for_site(latitude=51.75, longitude=-1.25, capacity_kwp=1.25, init_time=now)
 
     path = now.strftime("data/%Y/%-m/%-d/%Y%m%d_%H.csv")
