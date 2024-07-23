384d4113316cd6e20382f6e4f0e96f61f09f8796
Matthew Duffin <mduffin95@users.noreply.github.com>
2024-07-23 22:38:59 +0100

    use filesystem interface

--- scripts/hf_upload.py
+++ scripts/hf_upload.py
@@ -1,8 +1,7 @@
+import datetime
 import os
-
 from huggingface_hub import login
-from datasets import Dataset
-
+from huggingface_hub import HfFileSystem
 import forecast_csv
 
 if __name__ == "__main__":
@@ -10,7 +9,11 @@ if __name__ == "__main__":
     hf_token = os.getenv("HF_TOKEN")
     hf_repo = os.getenv("HF_REPO")
 
+    now = datetime.datetime.now()
+    forecast = forecast_csv.forecast_for_site(latitude=51.75, longitude=-1.25, capacity_kwp=1.25, init_time=now)
+
+    path = now.strftime("data/%Y/%-m/%-d/%Y%m%d_%H.csv")
     login(hf_token)
-    forecast = forecast_csv.forecast_for_site(latitude=51.75, longitude=-1.25, capacity_kwp=1.25)
-    ds = Dataset.from_pandas(forecast)
-    ds.push_to_hub(hf_repo)
+    fs = HfFileSystem()
+    with fs.open(f"datasets/{hf_repo}/{path}", "w") as f:
+        forecast.to_csv(path_or_buf=f)
