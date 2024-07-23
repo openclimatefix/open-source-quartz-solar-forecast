152561620b476e4f53252dbec8ab500f6b73c52c
Matthew Duffin <mduffin95@users.noreply.github.com>
2024-07-23 21:54:29 +0100

    update

--- scripts/hf_upload.py
+++ scripts/hf_upload.py
@@ -8,8 +8,9 @@ import forecast_csv
 if __name__ == "__main__":
 
     hf_token = os.getenv("HF_TOKEN")
+    hf_repo = os.getenv("HF_REPO")
+
     login(hf_token)
     forecast = forecast_csv.forecast_for_site(latitude=51.75, longitude=-1.25, capacity_kwp=1.25)
     ds = Dataset.from_pandas(forecast)
-    hf_repo = os.getenv("HF_REPO")
     ds.push_to_hub(hf_repo)
