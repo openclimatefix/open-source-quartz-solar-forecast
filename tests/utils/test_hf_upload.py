07f7b74309bbc047652bfe2e317f202698d476c0
Matthew Duffin <mduffin95@users.noreply.github.com>
2024-08-01 22:36:09 +0100

    fix test

--- tests/utils/test_hf_upload.py
+++ tests/utils/test_hf_upload.py
@@ -1,4 +1,4 @@
-from utils import hf_upload
+from quartz_solar_forecast.utils.hf_upload import get_file_path
 from datetime import datetime
 
 
@@ -7,6 +7,6 @@ def test_get_file_path():
     longitude = -1.25
     capacity_kwp = 1.25
     date = datetime(2024, 7, 26, 12, 0, 0)
-    path = hf_upload.get_file_path(latitude, longitude, capacity_kwp, "gb", date)
+    path = get_file_path(latitude, longitude, capacity_kwp, "gb", date)
 
     assert path == "data/2024/7/26/gb_51.75_-1.25_1.25_20240726_12.csv"
