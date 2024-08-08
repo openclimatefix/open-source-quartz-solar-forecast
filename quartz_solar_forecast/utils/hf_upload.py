50c6c6c9af086bb74049af330afd563a85a32c5e
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2024-08-08 22:40:49 +0530

    Publish forecast to Hugging Face at 9am every day  (#163) (#176)
    * new workflow
    
    * rename workflow
    
    * install
    
    * disable
    
    * no cd
    
    * publish from workflow
    
    * add env vars
    
    * requirements
    
    * loosen requirements
    
    * datasets only
    
    * datasets
    
    * lower datasets version
    
    * remove
    
    * wip
    
    * update
    
    * bump python version
    
    * pv-site-prediction
    
    * seems to be working
    
    * update
    
    * use filesystem interface
    
    * use UTC
    
    * set up cron schedule
    
    * don't need new requirements any more
    
    * tidy up
    
    * add lat/long to file name
    
    * loop through models
    
    * add capacity
    
    * add test for file path
    
    * new line at end of file
    
    * use named arg
    
    * change to HF_TOKEN_PUSH
    
    * wrong workflow
    
    * move to utils module
    
    * fix test
    
    * fix test
    
    * __init__.py
    
    * empty
    
    * update lat/long and capacity
    
    Co-authored-by: Matthew Duffin <mduffin95@users.noreply.github.com>

---
+++ quartz_solar_forecast/utils/hf_upload.py
@@ -0,0 +1,32 @@
+import os
+from datetime import datetime
+from huggingface_hub import login, HfFileSystem
+from quartz_solar_forecast.utils.forecast_csv import forecast_for_site
+
+
+def get_file_path(latitude: float,
+                  longitude: float,
+                  capacity_kwp: float,
+                  model: str = "gb",
+                  time: datetime = None) -> str:
+    return time.strftime(f"data/%Y/%-m/%-d/{model}_{latitude}_{longitude}_{capacity_kwp}_%Y%m%d_%H.csv")
+
+
+if __name__ == "__main__":
+
+    hf_token = os.getenv("HF_TOKEN")
+    hf_repo = os.getenv("HF_REPO")
+
+    login(hf_token)
+    fs = HfFileSystem()
+    now = datetime.utcnow()
+    latitude = 51.59,
+    longitude = -1.89
+    capacity_kwp = 4
+
+    for model in ["gb", "xgb"]:
+        forecast = forecast_for_site(latitude, longitude, capacity_kwp, model, now)
+
+        path = get_file_path(latitude, longitude, capacity_kwp, model, now)
+        with fs.open(f"datasets/{hf_repo}/{path}", "w") as f:
+            forecast.to_csv(path_or_buf=f)
