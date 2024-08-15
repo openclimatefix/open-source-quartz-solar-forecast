95d2d749e12c8dc30c4ee8534af4fa34df4d7c86
Matthew Duffin <mduffin95@users.noreply.github.com>
2024-08-15 23:18:27 +0100

    mock inverter docstring

--- quartz_solar_forecast/inverters/mock.py
+++ quartz_solar_forecast/inverters/mock.py
@@ -4,6 +4,9 @@ from quartz_solar_forecast.inverters.inverter import AbstractInverter
 
 
 class MockInverter(AbstractInverter):
+    """
+    Provides mock data when live data is not available.
+    """
 
     def get_data(self, ts: pd.Timestamp) -> pd.DataFrame:
         return pd.DataFrame(columns=['timestamp', 'power_kw'])
