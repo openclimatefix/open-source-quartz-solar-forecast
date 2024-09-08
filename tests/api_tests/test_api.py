509384365b6dfe692cbc5e2131b5fc203b8de06e
BraunRudolf <48672663+BraunRudolf@users.noreply.github.com>
2024-09-08 20:50:15 +0200

    Monkeypatch enphse env (#190)
    * Monkeypatch enphase env
    
    * Extraxt env variable from tests

--- tests/api_tests/test_api.py
+++ tests/api_tests/test_api.py
@@ -1,17 +1,18 @@
+import os
+
 import pytest
 from fastapi.testclient import TestClient
 
 from api.app.api import app
 
-
 expected_prediction_key = "power_kw"
-expected_dict_keys = ["timestamp", "predictions"] 
+expected_dict_keys = ["timestamp", "predictions"]
 prediction_key = expected_dict_keys[1]
 expected_number_of_values = 192
 expected_response_on_wrong_types = {
     "detail": [
         {
-            "loc": ["body","site", "latitude"],
+            "loc": ["body", "site", "latitude"],
             "msg": "Input should be less than or equal to 90",
             "type": "less_than_equal",
             "input": 91,
@@ -25,27 +26,34 @@ expected_response_on_wrong_types = {
             "ctx": {"ge": -180.0},
         },
         {
-            "loc": ["body","site", "capacity_kwp"],
+            "loc": ["body", "site", "capacity_kwp"],
             "msg": "Input should be greater than or equal to 0",
             "type": "greater_than_equal",
             "input": -1,
             "ctx": {"ge": 0.0},
         },
         {
-            "loc": ["body","site", "tilt"],
+            "loc": ["body", "site", "tilt"],
             "msg": "Input should be less than or equal to 90",
             "type": "less_than_equal",
             "input": 91,
             "ctx": {"le": 90.0},
         },
         {
-            "loc": ["body","site", "orientation"],
+            "loc": ["body", "site", "orientation"],
             "msg": "Input should be less than or equal to 360",
             "type": "less_than_equal",
             "input": 361,
             "ctx": {"le": 360.0},
         },
     ]
+}
+
+envs = {
+    "ENPHASE_CLIENT_ID": "1",
+    "ENPHASE_SYSTEM_ID": "1",
+    "ENPHASE_API_KEY": "test_key",
+    "ENPHASE_CLIENT_SECRET": "secret_secret",
 }
 
 
@@ -57,14 +65,14 @@ def client():
 @pytest.fixture
 def body():
     return {
-        "site":{
+        "site": {
             "latitude": -90,
             "longitude": -180,
             "capacity_kwp": 0,
             "tilt": 35,
             "orientation": 180,
             "inverter_type": "string",
-            },
+        },
         "timestamp": "2021-01-26 01:15:00",
     }
 
@@ -72,11 +80,11 @@ def body():
 @pytest.fixture
 def body_short():
     return {
-        "site":{
+        "site": {
             "latitude": -90,
             "longitude": -180,
             "capacity_kwp": 0,
-            },
+        },
         "timestamp": "2021-01-26 01:15:00",
     }
 
@@ -84,7 +92,7 @@ def body_short():
 @pytest.fixture
 def body_wrong():
     return {
-        "site":{
+        "site": {
             "latitude": 91,
             "longitude": -181,
             "capacity_kwp": -1,
@@ -95,21 +103,28 @@ def body_wrong():
         "timestamp": "2021-01-26 01:15:00",
     }
 
+
 @pytest.fixture
 def bad_redirect_url(client):
     return "url"
 
+
 @pytest.fixture
 def bad_redirect_url_correct_param(client):
     return "url?code=code"
+
 
 def test_api_ok(client, body):
     response = client.post("/forecast/", json=body)
     response_body = response.json()
     assert response.status_code == 200
     assert isinstance(response_body[prediction_key][expected_prediction_key], dict)
-    assert all([key in response_body.keys() for key in expected_dict_keys]), "Expected dictonary key is missing"
-    assert len(response_body[prediction_key][expected_prediction_key]) == expected_number_of_values, "Expected number of values is wrong"
+    assert all(
+        [key in response_body.keys() for key in expected_dict_keys]
+    ), "Expected dictonary key is missing"
+    assert (
+        len(response_body[prediction_key][expected_prediction_key]) == expected_number_of_values
+    ), "Expected number of values is wrong"
 
 
 def test_api_ok_short(client, body_short):
@@ -118,7 +133,9 @@ def test_api_ok_short(client, body_short):
 
     assert response.status_code == 200
     assert isinstance(response_body[prediction_key][expected_prediction_key], dict)
-    assert all([key in response_body.keys() for key in expected_dict_keys]), "Expected dictonary key is missing"
+    assert all(
+        [key in response_body.keys() for key in expected_dict_keys]
+    ), "Expected dictonary key is missing"
     assert len(response_body[prediction_key][expected_prediction_key]) == expected_number_of_values
 
 
@@ -130,23 +147,38 @@ def test_api_wrong_body(client, body_wrong):
     assert response.status_code == 422
     assert response_body == expected_response_on_wrong_types
 
-@pytest.mark.skip('There needs to be some extra mocking here')
-def test_getenphse_authorization_url(client):
+
+def test_getenphse_authorization_url(client, monkeypatch):
+
+    monkeypatch.setattr(os, "environ", envs)
+
     response = client.get("/solar_inverters/enphase/auth_url")
     assert response.status_code == 200
     assert isinstance(response.json()["auth_url"], str)
 
-@pytest.mark.skip('There needs to be some extra mocking here')
-def test_getenphse_token_and_system_id_bad_redirect_url(client, bad_redirect_url):
 
-    response = client.post("/solar_inverters/enphase/token_and_id", json={"redirect_url": bad_redirect_url})
+def test_getenphse_token_and_system_id_bad_redirect_url(client, bad_redirect_url, monkeypatch):
+
+    monkeypatch.setattr(os, "environ", envs)
+
+    response = client.post(
+        "/solar_inverters/enphase/token_and_id", json={"redirect_url": bad_redirect_url}
+    )
     response_body = response.json()
     assert response.status_code == 400
-    assert response_body['detail'] == 'Invalid redirect URL'
+    assert response_body["detail"] == "Invalid redirect URL"
 
-@pytest.mark.skip('There needs to be some extra mocking here')
-def test_getenphse_token_and_system_id_bad_redirect_url(client, bad_redirect_url_correct_param):
-    response = client.post("/solar_inverters/enphase/token_and_id", json={"redirect_url": bad_redirect_url_correct_param})
+
+def test_getenphse_token_and_system_id_bad_redirect_url(
+    client, bad_redirect_url_correct_param, monkeypatch
+):
+
+    monkeypatch.setattr(os, "environ", envs)
+
+    response = client.post(
+        "/solar_inverters/enphase/token_and_id",
+        json={"redirect_url": bad_redirect_url_correct_param},
+    )
     response_body = response.json()
     assert response.status_code == 400
-    assert  'Error getting access token and system ID: ' in response_body['detail']
+    assert "Error getting access token and system ID: " in response_body["detail"]
