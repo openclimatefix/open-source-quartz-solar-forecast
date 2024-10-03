02668f4b834c3596931af39aaf1382bce176133a
peterdudfield <peter.dudfield@hotmail.com>
2024-10-03 20:53:39 +0100

    make forecast work for large sites

--- quartz_solar_forecast/forecast.py
+++ quartz_solar_forecast/forecast.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import logging
 
 import pandas as pd
 
@@ -6,6 +7,7 @@ from quartz_solar_forecast.data import get_nwp, make_pv_data
 from quartz_solar_forecast.forecasts import forecast_v1_tilt_orientation, TryolabsSolarPowerPredictor
 from quartz_solar_forecast.pydantic_models import PVSite
 
+log = logging.getLogger(__name__)
 
 def predict_ocf(
     site: PVSite, model=None, ts: datetime | str = None, nwp_source: str = "icon"
@@ -25,12 +27,26 @@ def predict_ocf(
     if isinstance(ts, str):
         ts = datetime.fromisoformat(ts)
 
+    if site.capacity_kwp > 4:
+        log.warning("Your site capacity is greater than 4kWp, "
+                    "however the model is trained on sites with capacity <= 4kWp."
+                    "We therefore will run the model with a capacity of 4 kWp, "
+                    "and we'll scale the results afterwards.")
+        capacity_kwp_original = site.capacity_kwp
+        site.capacity_kwp = 4
+    else:
+        capacity_kwp_original = site.capacity_kwp
+
     # make pv and nwp data from nwp_source
     nwp_xr = get_nwp(site=site, ts=ts, nwp_source=nwp_source)
     pv_xr = make_pv_data(site=site, ts=ts)
 
     # load and run models
     pred_df = forecast_v1_tilt_orientation(nwp_source, nwp_xr, pv_xr, ts, model=model)
+
+    # scale the results if the capacity is different
+    if capacity_kwp_original != site.capacity_kwp:
+        pred_df["power_kw"] = pred_df["power_kw"] * capacity_kwp_original / site.capacity_kwp
 
     return pred_df
 
