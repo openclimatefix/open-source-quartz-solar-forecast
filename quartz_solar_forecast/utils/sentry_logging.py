beafdc77445bf17b2576c7601bcdad637481663c
peterdudfield <peter.dudfield@hotmail.com>
2024-12-10 08:26:28 +0000

    PR comment

--- quartz_solar_forecast/utils/sentry_logging.py
+++ quartz_solar_forecast/utils/sentry_logging.py
@@ -5,7 +5,7 @@ from quartz_solar_forecast.pydantic_models import PVSite
 
 import os
 
-quartz_solar_forecast_logging = bool(os.getenv("QUARTZ_SOLAR_FORECAST_LOGGING", True))
+quartz_solar_forecast_logging = os.getenv("QUARTZ_SOLAR_FORECAST_LOGGING", "True").lower() != "false"
 
 SENTRY_DSN = 'https://b2b6f3c97299f81464bc16ad0d516d0b@o400768.ingest.us.sentry.io/4508439933157376'
 sentry_sdk.init(
