499cebf33d94affd701ee310e80c8ec13ee5ab15
Harshvardhan Vatsa <76729417+hrsvrn@users.noreply.github.com>
2025-05-02 14:57:07 +0530

    modified forecast.py script (#279)

--- scripts/forecast_csv.py
+++ scripts/forecast_csv.py
@@ -1,11 +1,23 @@
+from quartz_solar_forecast.utils.forecast_csv import write_out_forecasts
+from datetime import datetime
+
 if __name__ == "__main__":
-    # please change the site name, start_datetime and end_datetime, latitude, longitude and capacity_kwp as per your requirement
+    site_name = input("Enter site name: ")
+    start_date_input = input("Enter start date (dd/mm/yyyy): ")
+    end_date_input = input("Enter end date (dd/mm/yyyy): ")
+    latitude = float(input("Enter latitude: "))
+    longitude = float(input("Enter longitude: "))
+    capacity_kwp = float(input("Enter capacity in kWp: "))
+
+    start_datetime = datetime.strptime(start_date_input, "%d/%m/%Y").strftime("%Y-%m-%d 00:00:00")
+    end_datetime = datetime.strptime(end_date_input, "%d/%m/%Y").strftime("%Y-%m-%d 00:00:00")
+
     write_out_forecasts(
         init_time_freq=6,
-        start_datetime="2024-03-10 00:00:00",
-        end_datetime="2024-03-11 00:00:00",
-        site_name="Test",
-        latitude=51.75,
-        longitude=-1.25,
-        capacity_kwp=1.25
+        start_datetime=start_datetime,
+        end_datetime=end_datetime,
+        site_name=site_name,
+        latitude=latitude,
+        longitude=longitude,
+        capacity_kwp=capacity_kwp
     )
