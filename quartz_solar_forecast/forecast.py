da015d57a82bd6a92a90d96c095f14d8de4a1f82
danishdynamic <shakeali.ali@gmail.com>
2026-03-13 11:26:02 +0530

    refactor: remove sentry logging and integration #349

--- quartz_solar_forecast/forecast.py
+++ quartz_solar_forecast/forecast.py
@@ -9,7 +9,6 @@ from quartz_solar_forecast.forecasts import (
     forecast_v1_tilt_orientation,
 )
 from quartz_solar_forecast.pydantic_models import PVSite
-from quartz_solar_forecast.utils.sentry_logging import write_sentry
 
 log = logging.getLogger(__name__)
 
@@ -146,10 +145,6 @@ def run_forecast(
     log.info(f"Running forecast for site at lat {site.latitude}, lon {site.longitude} "
               f"at time {ts} with model {model} and nwp source {nwp_source}")
 
-    # log usage to sentry, if you dont want to log usage to sentry, you can
-    # 1. set environmental variable QUARTZ_SOLAR_FORECAST_LOGGING='false', or
-    # 2. comment out this line
-    write_sentry({"site": site.copy(), "model": model, "ts": ts, "nwp_source": nwp_source})
 
     if model == "gb":
         return predict_ocf(site, None, ts, nwp_source, live_generation)
