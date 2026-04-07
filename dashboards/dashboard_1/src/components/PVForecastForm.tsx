bc2825856b1755cfb8ae781244bd23fcde2c487d
Saksham Mawari <sakshammawari@gmail.com>
2026-04-07 20:33:59 +0530

    Changed the stringify value

--- dashboards/dashboard_1/src/components/PVForecastForm.tsx
+++ dashboards/dashboard_1/src/components/PVForecastForm.tsx
@@ -54,7 +54,7 @@ export function PVForecastForm({ updatePredictions }) {
     const response = await fetch(`http://localhost:8000/forecast`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(values),
+      body: JSON.stringify({site:values}),
     });
     const data = await response.json();
     updatePredictions(data.power_kw);
