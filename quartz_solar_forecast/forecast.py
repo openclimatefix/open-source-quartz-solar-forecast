d9b3c9552ed262d6fae4f7d051079b0c69893db1
Sneh <snehkansagara@gmail.com>
2026-05-05 14:41:35 +0530

    fixed (#329)

--- quartz_solar_forecast/forecast.py
+++ quartz_solar_forecast/forecast.py
@@ -82,11 +82,13 @@ def predict_tryolabs(site: PVSite, ts: datetime | str = None):
 
     # set start and end time, if no time is given use current time
     if ts is None:
-        start_date = pd.Timestamp.now().strftime("%Y-%m-%d")
-        start_time = pd.Timestamp.now().round(freq="h")
+        now = pd.Timestamp.now()
+        start_date = now.strftime("%Y-%m-%d")
+        start_time = now.round(freq="h")
     else:
-        start_date = pd.Timestamp(ts).strftime("%Y-%m-%d")
-        start_time = pd.Timestamp(ts).round(freq="h")
+        ts_pd = pd.Timestamp(ts)
+        start_date = ts_pd.strftime("%Y-%m-%d")
+        start_time = ts_pd.round(freq="h")
 
     end_time = start_time + pd.Timedelta(hours=48)
     start_date_datetime = datetime.strptime(start_date, "%Y-%m-%d")
